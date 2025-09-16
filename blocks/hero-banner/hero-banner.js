2560f047fc574b96f4014c04b9abfbefa106dc36
Edward Shih <edwards@ensemble.com>
2025-09-16 11:20:49 -0700

    files required for module 3

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -1,15 +1,19 @@
 export default function decorate(block) {
+  // Destructures the picture and text rows
+  // first - first row => image
+  // second - second row => text
   const [first, second] = block.children;
 
+  // find the picture in the first row
+  // find the text in the second row
   const picture = first.querySelector('picture');
   const textContainer = second.firstElementChild;
 
+  // clear the block html, we're going to rebuild it
   block.innerHTML = '';
-
   if (picture) {
     block.append(picture);
   }
-
   if (textContainer) {
     textContainer.classList.add('hero-text');
     block.append(textContainer);
