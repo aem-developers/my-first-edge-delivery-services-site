06f24025a5c472ea5aeafd56aa769d0ca3cfbb99
Edward Shih <edwards@ensemble.com>
2025-09-25 11:42:13 -0700

    files required for module-4

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -18,9 +18,6 @@ export default function decorate(block) {
   const rawImage = document.createElement('img');
   rawImage.src = 'https://main--my-first-edge-delivery-services-site--aem-developers.aem.page/module-4/images/media_1db2e63a4c1003f5ac8733049f06a980689d901b4.jpg';
   rawImage.alt = 'Unoptimized LCP test image';
-  rawImage.loading = 'lazy';
-  rawImage.width = 1600;
-  rawImage.height = 1066;
 
   block.append(rawImage);
   if (picture) {
